8ee78133d2cc4abd0685682fd04a75774e69d9aa
Skillerde6de <mennobordes@hetnet.nl>
2019-05-22 22:37:57 +0200

    Added basic explanation for the webshop

--- src/pages/portfolio/artic_webshop.js
+++ src/pages/portfolio/artic_webshop.js
@@ -15,6 +15,23 @@ const Artic_webshop = props => (
           <header className="major">
             <h1>Artic webshop</h1>
           </header>
+          <p>
+            In het tweede jaar van mijn Informatica opleiding werd onze klas in
+            groepen opgedeeld, waarna elke groep de opdracht kreeg om een
+            webshop te creëren. Na overleg hebben wij ervoor gekozen om een
+            webshop te maken waarop kunst wordt aangeboden. Alle kunstwerken die
+            onze webshop zou gaan aanbieden, zijn afkomstig van het Rijksmuseum.
+          </p>
+          <p>
+            Alvorens te beginnen met het maken van de website, hebben we eerst
+            een mockup gemaakt. Deze mockup dient om de Product Owner (PO) een
+            indruk te geven over wat wij als groep in gedachten hebben om te
+            maken.
+          </p>
+          <p>
+            Na goedkeuring van de PO zijn we begonnen met het creëren van de
+            webshop.
+          </p>
         </div>
       </section>
     </div>
