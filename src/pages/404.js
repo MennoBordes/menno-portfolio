641db87004f71494ba09ed504147790183bc98c6
Skillerde6de <mennobordes@hetnet.nl>
2019-05-24 15:54:20 +0200

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,12 +4,12 @@ import Layout from '../components/layout'
 const NotFoundPage = () => (
   <Layout>
     <div id="main" className="alt">
-        <section id="one">
-            <div className="inner">
-                <h1>NOT FOUND</h1>
-                <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-            </div>
-        </section>
+      <section id="one">
+        <div className="inner">
+          <h1>Pagina niet gevonden</h1>
+          <p>Helaas, deze pagina bestaat niet.</p>
+        </div>
+      </section>
     </div>
   </Layout>
 )
