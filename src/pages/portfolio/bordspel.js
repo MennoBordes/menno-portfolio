ae6a8af9142f2daefb98984ec3c4dc33549d1680
Skillerde6de <mennobordes@hetnet.nl>
2019-05-29 18:55:51 +0200

    Changed layout of bordspel

--- src/pages/portfolio/bordspel.js
+++ src/pages/portfolio/bordspel.js
@@ -26,56 +26,55 @@ const bordspel = props => (
             <p>25 mei 2019</p>
           </header>
 
-          {/* Content */}
-          <div className="grid-wrapper">
-            {/* Intro */}
-            <div className="col-9">
-              <p>
-                Tijdens de eerste 10 weken van 1e schooljaar op het HBO was er
-                de opdracht om als groep een bordspel te ontwikkelen met als
-                thema Rotterdam. Met de groep waarmee ik heb samengewerkt,
-                hebben we na overleg besloten om het bordspel te maken over een
-                mythisch Rotterdam.
-              </p>
-              <p>
-                <span className="image right">
-                  <img src={Bordspel_naam} alt="Bordspel naam" />
-                </span>
-                De bedoeling was om mythische creaturen en historische personen
-                te gebruiken om een uniek bordspel te maken. Dit bordspel hebben
-                we na overleg met de klant de naam{' '}
-                <b>"De strijd der Districten”</b> gegeven.
-              </p>
-              {/* <p>
-                Bij de inleiding van het spel wordt verteld dat professor
-                Putricide per ongeluk de duistere portaal opende. Hierdoor werd
-                de huidige samenleving verwoest. Alleen wanneer één district
-                zijn 3 torens weet te bouwen, dan pas, zal alles weer normaal
-                worden.
-              </p> */}
-            </div>
-            <div className="col-3 image fit">
-              <img src={img_bordspel} alt="Bordspel bord" />
-              <sup>Het uiterlijk van het bordspel.</sup>
-            </div>
-
+          {/* Intro */}
+          <section className="spotlights">
+            <section id="intro">
+              <div className="image">
+                <img src={img_bordspel} alt="Bordspel bord" />
+                {/* <sup>Het uiterlijk van het bordspel.</sup> */}
+              </div>
+              <div className="content">
+                <div className="inner">
+                  <header className="major">
+                    <h3>Intro</h3>
+                  </header>
+                  <p>
+                    Tijdens de eerste 10 weken van het eerste schooljaar op het
+                    HBO was er de opdracht om als groep een bordspel te
+                    ontwikkelen met als thema Rotterdam. Met de groep waarmee ik
+                    heb samengewerkt, hebben we na overleg besloten om het
+                    bordspel te maken met als thema <i>mythisch Rotterdam</i>.
+                  </p>
+                  <p>
+                    <span className="image right">
+                      <img src={Bordspel_naam} alt="Bordspel naam" />
+                    </span>
+                    De bedoeling was om mythische creaturen en historische
+                    personen te gebruiken om een uniek bordspel te maken. Dit
+                    bordspel hebben we na overleg met de klant de naam{' '}
+                    <b>"De strijd der Districten”</b> gegeven.
+                  </p>
+                </div>
+              </div>
+            </section>
             {/* Film */}
-            <div className="col-12">
+            <div>
               <header className="major">
-                <h3>Introductie</h3>
+                <h3>Film</h3>
               </header>
               <Video
                 videoSrcURL="https://www.youtube.com/embed/MwgGU0DmAp4"
                 videoTitle="Bordspel introductie"
               />
             </div>
+          </section>
 
-            {/* Uitleg */}
-            <div className="col-12 grid-wrapper">
-              {/* Header */}
+          {/* Spelregels */}
+          <div className="grid-wrapper">
+            <div className="col-12 grid-wrapper" id="spelregels">
               <div className="col-12">
                 <header className="major">
-                  <h3>Uitleg</h3>
+                  <h3>Spelregels</h3>
                 </header>
               </div>
 
@@ -106,6 +105,7 @@ const bordspel = props => (
                 </p>
               </div>
 
+              {/* Resources */}
               {/* Dobbelstenen */}
 
               {/* Vakjes */}
@@ -115,100 +115,109 @@ const bordspel = props => (
               {/* Kanskaarten */}
 
               {/* Helden */}
-              <div className="col-12">
-                <p>
-                  Elk district heeft een eigen held, en elke held heeft andere
-                  eigenschappen. De eigenschappen en bouwkosten worden hieronder
-                  aangegeven.
-                </p>
-              </div>
+              <div className="col-12 grid-wrapper" id="helden">
+                <div className="col-12">
+                  <header className="major">
+                    <h4>Helden</h4>
+                  </header>
+                  <p>
+                    Elk district heeft een eigen held, en elke held heeft andere
+                    eigenschappen. De eigenschappen en bouwkosten worden
+                    hieronder aangegeven.
+                  </p>
+                </div>
 
-              {/* Textiel */}
-              <div className="col-3">
-                <h3>Goud</h3>
-                <p>Frits Goldschmeding</p>
-                <h5>Eigenschappen</h5>
-                <ul>
-                  <li>
-                    Als je op een portaal vak land, hoef je de actie niet uit te
-                    voeren.
-                  </li>
-                  <li>
-                    Als je op een belasting vak land, hoef je geen belasting te
-                    betalen.
-                  </li>
-                </ul>
-                <h5>Bouwkosten</h5>
-                <ol>
-                  <li>Verdieping 1 = 1 goud</li>
-                  <li>Verdieping 2 = 1 goud &amp; 1 voedsel</li>
-                  <li>Verdieping 3 = 1 klei &amp; 1 voedsel &amp; 1 textiel</li>
-                </ol>
-              </div>
+                {/* Textiel */}
+                <div className="col-3">
+                  <h3>Goud</h3>
+                  <p>Frits Goldschmeding</p>
+                  <h5>Eigenschappen</h5>
+                  <ul>
+                    <li>
+                      Als je op een portaal vak land, hoef je de actie niet uit
+                      te voeren.
+                    </li>
+                    <li>
+                      Als je op een belasting vak land, hoef je geen belasting
+                      te betalen.
+                    </li>
+                  </ul>
+                  <h5>Bouwkosten</h5>
+                  <ol>
+                    <li>Verdieping 1 = 1 goud</li>
+                    <li>Verdieping 2 = 1 goud &amp; 1 voedsel</li>
+                    <li>
+                      Verdieping 3 = 1 klei &amp; 1 voedsel &amp; 1 textiel
+                    </li>
+                  </ol>
+                </div>
 
-              {/* Klei */}
-              <div className="col-3">
-                <h3>Klei</h3>
-                <p>Meisje van Yde</p>
-                <h5>Eigenschappen</h5>
-                <ul>
-                  <li>
-                    Kan zowel op investeer vakken als op resource vakken
-                    investeren.
-                  </li>
-                  <li>
-                    Als je op het markthal vak land hoef je maar 1 resource
-                    kaart in te leveren, je mag zelf kiezen welke.
-                  </li>
-                </ul>
-                <h5>Bouwkosten</h5>
-                <ol>
-                  <li>Verdieping 1 = 1 klei</li>
-                  <li>Verdieping 2 = 1 klei &amp; 1 textiel</li>
-                  <li>Verdieping 3 = 1 goud &amp; 1 voedsel &amp; 1 textiel</li>
-                </ol>
-              </div>
+                {/* Klei */}
+                <div className="col-3">
+                  <h3>Klei</h3>
+                  <p>Meisje van Yde</p>
+                  <h5>Eigenschappen</h5>
+                  <ul>
+                    <li>
+                      Kan zowel op investeer vakken als op grondstof vakken
+                      investeren.
+                    </li>
+                    <li>
+                      Als je op het markthal vak land hoef je maar 1 grondstof
+                      kaart in te leveren, je mag zelf kiezen welke.
+                    </li>
+                  </ul>
+                  <h5>Bouwkosten</h5>
+                  <ol>
+                    <li>Verdieping 1 = 1 klei</li>
+                    <li>Verdieping 2 = 1 klei &amp; 1 textiel</li>
+                    <li>
+                      Verdieping 3 = 1 goud &amp; 1 voedsel &amp; 1 textiel
+                    </li>
+                  </ol>
+                </div>
 
-              {/* Voedsel */}
-              <div className="col-3">
-                <h3>Voedsel</h3>
-                <p>Michiel de Ruyter</p>
-                <h5>Eigenschappen</h5>
-                <ul>
-                  <li>
-                    Bij elke worp van de transport dobbelsteen <b>mag</b> je het
-                    dubbele aantal ogen lopen
-                  </li>
-                  <li>
-                    Als je op het markthal vak land hoef je maar 1 resource
-                    kaart in te leveren, je mag zelf kiezen welke.
-                  </li>
-                </ul>
-                <h5>Bouwkosten</h5>
-                <ol>
-                  <li>Verdieping 1 = 1 voedsel</li>
-                  <li>Verdieping 2 = 1 voedsel &amp; 1 goud</li>
-                  <li>Verdieping 3 = 1 goud &amp; 1 klei &amp; 1 textiel</li>
-                </ol>
-              </div>
+                {/* Voedsel */}
+                <div className="col-3">
+                  <h3>Voedsel</h3>
+                  <p>Michiel de Ruyter</p>
+                  <h5>Eigenschappen</h5>
+                  <ul>
+                    <li>
+                      Bij elke worp van de transport dobbelsteen <b>mag</b> je
+                      het dubbele aantal ogen lopen
+                    </li>
+                    <li>
+                      Als je op het markthal vak land hoef je maar 1 grondstof
+                      kaart in te leveren, je mag zelf kiezen welke.
+                    </li>
+                  </ul>
+                  <h5>Bouwkosten</h5>
+                  <ol>
+                    <li>Verdieping 1 = 1 voedsel</li>
+                    <li>Verdieping 2 = 1 voedsel &amp; 1 goud</li>
+                    <li>Verdieping 3 = 1 goud &amp; 1 klei &amp; 1 textiel</li>
+                  </ol>
+                </div>
 
-              {/* Textiel */}
-              <div className="col-3">
-                <h3>Textiel</h3>
-                <p>Oger Lusink</p>
-                <h5>Eigenschappen</h5>
-                <ul>
-                  <li>
-                    Kom je op een resource vak dan mag de resource dobbelsteen
-                    twee maal geworpen worden i.p.v. één keer.
-                  </li>
-                </ul>
-                <h5>Bouwkosten</h5>
-                <ol>
-                  <li>Verdieping 1 = 1 textiel</li>
-                  <li>Verdieping 2 = 1 textiel &amp; 1 klei</li>
-                  <li>Verdieping 3 = 1 goud &amp; 1 klei &amp; 1 voedsel</li>
-                </ol>
+                {/* Textiel */}
+                <div className="col-3">
+                  <h3>Textiel</h3>
+                  <p>Oger Lusink</p>
+                  <h5>Eigenschappen</h5>
+                  <ul>
+                    <li>
+                      Kom je op een grondstof vak dan mag de grondstof
+                      dobbelsteen twee maal geworpen worden i.p.v. één keer.
+                    </li>
+                  </ul>
+                  <h5>Bouwkosten</h5>
+                  <ol>
+                    <li>Verdieping 1 = 1 textiel</li>
+                    <li>Verdieping 2 = 1 textiel &amp; 1 klei</li>
+                    <li>Verdieping 3 = 1 goud &amp; 1 klei &amp; 1 voedsel</li>
+                  </ol>
+                </div>
               </div>
             </div>
           </div>
