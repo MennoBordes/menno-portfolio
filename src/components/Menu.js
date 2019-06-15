bf6895e0ab321d451471ae4ad0e9cedbe1c01ee4
Skillerde6de <mennobordes@hetnet.nl>
2019-06-15 23:12:19 +0200

    Added my CV

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -17,6 +17,11 @@ const Menu = props => (
           </Link>
         </li>
         <li>
+          <Link onClick={props.onToggleMenu} to="/cv">
+            CV
+          </Link>
+        </li>
+        <li>
           <Link onClick={props.onToggleMenu} to="/over">
             Over
           </Link>
---
+++ src/pages/cv.js
@@ -0,0 +1,160 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+// Components
+import Layout from '../components/layout'
+import BannerLanding from '../components/BannerLanding'
+
+const over = props => (
+  <Layout>
+    <Helmet>
+      <title>CV - M.Bordes</title>
+      <meta name="descritpion" content="CV" />
+    </Helmet>
+
+    <BannerLanding />
+    <div id="main" className="alt">
+      <div className="inner">
+        <header className="major">
+          <h1>CV</h1>
+        </header>
+        {/* WhoAmI */}
+        <section>
+          <table>
+            <tbody>
+              <tr>
+                <td>Naam</td>
+                <td>Menno Bordes</td>
+              </tr>
+              <tr>
+                <td>Geboortedatum</td>
+                <td>04-09-1997</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        {/* Opleidingen */}
+        <section>
+          <h2>Opleidingen</h2>
+          <table>
+            <tbody>
+              {/* Hogeschool Rotterdam */}
+              <tr>
+                <td>Software engineer informatica</td>
+                <td>Huidige opleiding</td>
+                <td>Hogeschool Rotterdam</td>
+              </tr>
+              <ul>
+                <li>C&#35;</li>
+                <li>Javascript</li>
+                <li>Python</li>
+                <li>Visual Basic</li>
+                <li>MS SQL &#38; MY SQL</li>
+              </ul>
+            </tbody>
+            {/* ID college */}
+            <tbody>
+              <tr>
+                <td>Middenkader Engineering Elektrotechniek</td>
+                <td>Huidige opleiding</td>
+                <td>Hogeschool Rotterdam</td>
+              </tr>
+              <ul>
+                <li>
+                  Certificate CCNA part 1 Introduction (introduction to
+                  networks)
+                </li>
+                <li>Programmeren van Microcontrollers / Arduino</li>
+                <li>Kennis van Autocad</li>
+                <li>VCA diploma (Behaald december 2013)</li>
+              </ul>
+            </tbody>
+            {/* Erasmus college */}
+            <tbody>
+              <tr>
+                <td>Mavo</td>
+                <td>Juli 2013</td>
+                <td>Erasmus College</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        {/* Werkervaring */}
+        <section>
+          <h2>Werkervaring</h2>
+          {/* Intratuin */}
+          <div id="Intratuin-werk">
+            <p>
+              <strong>
+                Verkoop medewerker |{' '}
+                <a href="https://www.intratuin.nl/winkels/intratuin-zevenhuizen">
+                  Intratuin
+                </a>{' '}
+                Zevenhuizen | september 2017 - mei 2019
+              </strong>
+            </p>
+            <p>Medewerker in verkoop van goederen</p>
+          </div>
+          {/* Unica stage */}
+          <div id="Unica-stage">
+            <p>
+              <strong>
+                Eindstage MBO | <a href="https://www.unica.nl/">Unica BV</a>{' '}
+                Bodegraven | februari 2017 - juli 2017
+              </strong>
+            </p>
+            <p>
+              Een applicatie gemaakt in Visual Basic met Windows forms, welke
+              gegevens van verlichting uitleest en hiervan de data weergeeft.
+            </p>
+          </div>
+          {/* Trescal werk */}
+          <div id="Trescal-werk">
+            <p>
+              <strong>
+                Technisch medewerker |{' '}
+                <a href="http://www.trescal.com/nl_BE/">Trescal BV</a>{' '}
+                Zoetermeer | 2016 - januari 2017
+              </strong>
+            </p>
+            <p>Kalibreren en repareren van meetapparatuur</p>
+          </div>
+          {/* Trescal stage */}
+          <div id="Trescal-stage">
+            <p>
+              <strong>
+                Technisch stagiair |{' '}
+                <a href="http://www.trescal.com/nl_BE/">Trescal BV</a>{' '}
+                Zoetermeer | 2015 - 2016
+              </strong>
+            </p>
+            <p>Kalibreren van meetapparatuur</p>
+          </div>
+          {/* DME stage */}
+          <div id="DME-stage">
+            <p>
+              <strong>
+                Technisch stagiair |{' '}
+                <a href="http://www.dme.nl/">DME Installatiegroep</a>{' '}
+                Moerkapelle | 2014 - 2015
+              </strong>
+            </p>
+            <p>Installatie van elektrische systemen in woningen en bedrijven</p>
+          </div>
+          {/* Tas tomaten */}
+          <div id="Tas-tomaten-werk">
+            <p>
+              <strong>
+                Tuinbouw medewerker |{' '}
+                <a href="https://www.tastomaten.nl/">Tas tomaten Zevenhuizen</a>{' '}
+                | 2010 - 2015
+              </strong>
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default over
