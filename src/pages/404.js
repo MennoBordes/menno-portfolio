d2b310c73e7ff7622a0f7c3e3efc6ab71737ced8
Skillerde6de <mennobordes@hetnet.nl>
2019-06-18 20:54:27 +0200

    Added redirect to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,6 +10,9 @@ const NotFoundPage = () => (
         <div className="inner">
           <h1>Pagina niet gevonden</h1>
           <p>Helaas, deze pagina bestaat niet.</p>
+          <a href="/" className="button special">
+            Klik hier om terug te gaan naar het beginscherm.
+          </a>
         </div>
       </section>
     </div>
