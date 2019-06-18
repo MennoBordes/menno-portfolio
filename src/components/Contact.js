99110a4f324882d26914b1f31a1afbadc4802527
Skillerde6de <mennobordes@hetnet.nl>
2019-06-18 17:07:11 +0200

    Removed form and tidied up contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,65 +4,7 @@ const Contact = props => (
   <section id="contact">
     <div className="inner">
       <div />
-      {/* <section>
-        <form name="contact" method="POST" data-netlify="true" action="/">
-          <div className="field half first">
-            <label htmlFor="name">naam</label>
-            <input type="text" name="name" id="name" />
-          </div>
-          <div className="field half">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
-          </div>
-          <div className="field">
-            <label htmlFor="message">Bericht</label>
-            <textarea name="message" id="message" rows="6" />
-          </div>
-          <ul className="actions">
-            <li>
-              <input type="submit" value="Send Message" className="special" />
-            </li>
-            <li>
-              <input type="reset" value="Clear" />
-            </li>
-          </ul>
-        </form>
-      </section> */}
-      <section>
-        <form
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <p className="hidden">
-            <label>
-              Don&#39;t fill this out if you are human:{' '}
-              <input name="bot-field" />
-            </label>
-          </p>
-          <div className="field half first">
-            <label htmlFor="name">naam</label>
-            <input type="text" name="name" id="name" />
-          </div>
-          <div className="field half">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
-          </div>
-          <div className="field">
-            <label htmlFor="message">Bericht</label>
-            <textarea name="message" id="message" rows="6" />
-          </div>
-          <ul className="actions">
-            <li>
-              <input type="submit" value="Send Message" className="special" />
-            </li>
-            <li>
-              <input type="reset" value="Clear" />
-            </li>
-          </ul>
-        </form>
-      </section>
+      {/* Contact */}
       <section className="split">
         <section>
           <div className="contact-method">
@@ -81,19 +23,6 @@ const Contact = props => (
             <span className="icon alt fa-phone" />
             <h3>Telefoon</h3>
             <span>+31 (0)6 58949191</span>
-          </div>
-        </section>
-        <section>
-          <div className="contact-method">
-            <span className="icon alt fa-home" />
-            <h3>Address</h3>
-            <span>
-              1234 Somewhere Road #5432
-              <br />
-              Nashville, TN 00000
-              <br />
-              United States of America
-            </span>
           </div>
         </section>
       </section> */}
